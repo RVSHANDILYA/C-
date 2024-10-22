8570a99e293dc09769a5fef016e5c63b7dbacd8d
RVShandilya <shandilyapuram544@gail.com>
2024-10-22 13:15:10 +0530

    Input-output-operations

---
+++ inputs_outputs.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"\n Enter 1st number:";
+    cin>>a;
+    cout<<"\n Enter 2nd number:";
+    cin>>b;
+    
+    cout<<"\n Addition of entered two numbers is:"<< a+b;
+    cout<<"\n Subtraction of entered two numbers is:"<< a-b;
+    cout<<"\n Division of entered two numbers is:" << a/b;
+    cout<<"\n Multiplication of entered two numbers is:"<< a*b;
+    cout<<"\n Modulus(Remainder) of entered two numbers is:"<< a%b;
+
+
+}
